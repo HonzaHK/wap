85aa52e45d3ecc093764e9f2b32f7003bd437cca
HonzaHK <zgupie@gmail.com>
2017-02-28 17:52:44 +0100

    added top-level component: wapCalc

--- wap.js
+++ wap.js
@@ -23,20 +23,23 @@ createTableElem = function(row,col){
 	return t;
 }
 
-createCellContElem = function(){
+createCellCont = function(){
 
 	var cc = {};
 
 	cc.c_text = document.createElement("input");
 	cc.c_text.setAttribute("type","text");
+	cc.c_text.classList.add("ccInput");
 
-	cc.c_text.addEventListener("input",function (e) {
-		wt.setCurrentCellText(this.value);
-	});
+	cc.formula_re = /^=(ADD|MUL|SUB|DIV|AVG)\(\${0,1}[0-9]+[\${0,1}[0-9]+]*\)$/m
+	console.log(cc.formula_re.test("=AVG(099)"))
 
 	cc.elem = document.createElement("div");
 	cc.elem.appendChild(cc.c_text)
 
+	cc.getInputText = function(){
+		return cc.c_text.value;
+	}
 	cc.setInputText = function(text){
 		cc.c_text.value = text;
 	}
@@ -45,97 +48,112 @@ createCellContElem = function(){
 	return cc;
 }
 
+createCalcElem = function(){
+	var c = document.createElement("div");
+	c.classList.add("wapCalc");
+	return c;
+}
+
 const KEYc_LEFT=37, KEYc_UP=38, KEYc_RIGHT=39, KEYc_DOWN=40;
 document.onkeydown = function(e){
 	if(e.keyCode==KEYc_LEFT || e.keyCode==KEYc_UP || e.keyCode==KEYc_RIGHT || e.keyCode==KEYc_DOWN){
-		wt.keyDown(e.keyCode);
+		wc.keyDown(e.keyCode);
 		return false;
 	}
 	
 	return true;
 };
 
-var wt = {
-	r_cnt: 0,
-	c_cnt: 0,
-	elem: null,
+getWapTable = function(r,c) {
+	return {
 
-	r_curr: 0,
-	c_curr: 0,
-	keyDown: function(keyCode){
-		var newRow=wt.r_curr, newCol=wt.c_curr;
+		r_cnt: r,
+		c_cnt: c,
+		elem: createTableElem(r,c),
 
-		if (keyCode == KEYc_LEFT && wt.c_curr!=0) {
-			newCol--;
-		}
-		else if (keyCode == KEYc_UP && wt.r_curr!=0) { 
-			newRow--;
-		}
-		else if (keyCode == KEYc_RIGHT && wt.c_curr!=wt.c_cnt-1) { 
-			newCol++;
-		}
-		else if (keyCode == KEYc_DOWN && wt.r_curr!=wt.r_cnt-1) { 
-			newRow++;
-		}
-		else{
-			return;
-		}
+		r_curr: 0,
+		c_curr: 0,
+		keyDown: function(keyCode){
+			var newRow=this.r_curr, newCol=this.c_curr;
 
-		wt.setCurrentCell(newRow,newCol);
-	},
-	getCell: function(r,c){
-		return wt.elem.rows[r].cells[c];
-	},
-	getCurrentCell: function(){
-		return wt.getCell(wt.r_curr,wt.c_curr);
-	},
-	setCurrentCell: function(r,c){
-		var curr_cell = wt.getCurrentCell();
-		curr_cell.classList.toggle("currentCell");
-		
-		wt.forceSetCurrentCell(r,c);
-	},
-	forceSetCurrentCell: function(r,c){
-		tgt_cell = wt.getCell(r,c);
-		tgt_cell.classList.toggle("currentCell");
-		wt.r_curr = r;
-		wt.c_curr = c;
-
-		wt.cell_cont.setInputText(tgt_cell.innerHTML);
-	},
-	setCurrentCellText: function(text){
-		var curr_cell = wt.getCurrentCell();
-		curr_cell.innerHTML = text;
-	},
-	initCurrentCell: function() {
-		var t = wt.elem;
-		for(var r=0;r<t.r_cnt;r++){
-			for(var c=0;c<tr.c_cnt;c++){
-				tgt_cell = wt.getCell(r,c);
-				tgt_cell.classList.remove("currentCell");
+			if (keyCode == KEYc_LEFT && this.c_curr!=0) {
+				newCol--;
 			}
+			else if (keyCode == KEYc_UP && this.r_curr!=0) { 
+				newRow--;
+			}
+			else if (keyCode == KEYc_RIGHT && this.c_curr!=this.c_cnt-1) { 
+				newCol++;
+			}
+			else if (keyCode == KEYc_DOWN && this.r_curr!=this.r_cnt-1) { 
+				newRow++;
+			}
+			else{
+				return false;
+			}
+
+			this.setCurrentCell(newRow,newCol);
+			return true;
+		},
+		getCell: function(r,c){
+			return this.elem.rows[r].cells[c];
+		},
+		getCurrentCell: function(){
+			return this.getCell(this.r_curr,this.c_curr);
+		},
+		setCurrentCell: function(r,c){
+			var curr_cell = this.getCurrentCell();
+			curr_cell.classList.remove("currentCell");
+			tgt_cell = this.getCell(r,c);
+			tgt_cell.classList.add("currentCell");
+			this.r_curr = r;
+			this.c_curr = c;
+		},
+		getCurrentCellText: function(){
+			var curr_cell = this.getCurrentCell();
+			return curr_cell.innerHTML;
+		},
+		setCurrentCellText: function(text){
+			var curr_cell = this.getCurrentCell();
+			curr_cell.innerHTML = text;
+		}
+	}
+}
+
+getWapCalc = function(r,c){
+	wc = {}
+	wc.elem = createCalcElem();
+	
+	wc.t = getWapTable(r,c);
+
+	wc.cc = createCellCont();
+	wc.elem.appendChild(wc.cc.elem);
+	wc.elem.appendChild(wc.t.elem);
+
+	wc.keyDown= function(keyCode){
+		if(wc.t.keyDown(keyCode)){
+			wc.cc.setInputText(wc.t.getCurrentCellText());
+			return true;
 		}
 
-		wt.forceSetCurrentCell(0,0);
+		return false;
+	};
+
+	wc.cc.c_text.addEventListener("input",function (e) {
+		wc.t.setCurrentCellText(wc.cc.getInputText());
+	});
+
+	wc.init= function(){
+		wc.t.setCurrentCell(0,0);
+		wc.cc.setInputText(wc.t.getCurrentCellText());
 	}
 
-
+	return wc;
 }
-
-initTable = function(wt,r,c){
-	wt.r_cnt= r;
-	wt.c_cnt= c;
-	wt.elem= createTableElem(r,c);
-	wt.cell_cont= createCellContElem();
-
-	wt.initCurrentCell();
-}
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
 	var container = document.getElementById("container");
-	initTable(wt,5,3);
-	container.appendChild(wt.elem);
-	container.appendChild(wt.cell_cont.elem);
+	var wc = getWapCalc(5,3);
+	wc.init();
+	container.appendChild(wc.elem);
 });
