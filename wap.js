1b8569ffecf2cec7e8b1a54d6c0cf8f36d77b4c5
HonzaHK <zgupie@gmail.com>
2017-03-30 23:35:25 +0200

    sum and avg

--- wap.js
+++ wap.js
@@ -1,3 +1,6 @@
+//VUTBR FIT WAP - Tabulkovy kalkulator
+//Author: Jan Kubis/xkubis13
+
 rand = function(){
 	return Math.floor(Math.random() * 10) + 1 ; 
 }
@@ -51,7 +54,10 @@ createCellCont = function(){
 }
 
 
-createTableElem = function(row,col){
+createTableElem = function(rows){
+	var row = rows.length;
+	var col = rows[0].cells.length;
+	
 	var t = document.createElement("table");
 	t.setAttribute("class", "wapTable");
 	var tr = document.createElement("tr"); //table row for headers
@@ -71,14 +77,9 @@ createTableElem = function(row,col){
 		tr.appendChild(trh);
 		for(var c=0;c<col;c++){
 			var td = document.createElement("td");
-			td.innerHTML = rand();
-			if(r==0 && c==0){
-				td.innerHTML = "=c3+6+b1"
-			}
-			td.setAttribute("class", "wapCell");
-			td.setAttribute("data-row", ""+r);
-			td.setAttribute("data-col", ""+c);
-			tr.appendChild(td);
+			var cellData = rows[r].cells[c];
+			td.innerHTML = cellData.inputText;
+			tr.appendChild(cellData.elem);
 		}
 		t.appendChild(tr);
 	}
@@ -86,117 +87,217 @@ createTableElem = function(row,col){
 	return t;
 }
 
+createCellElem = function(cell){
+	var td = document.createElement("td");
+	td.innerHTML = cell.inputText;
+	td.setAttribute("class", "wapCell");
+	td.setAttribute("data-row", ""+cell.r);
+	td.setAttribute("data-col", ""+cell.c);
+
+	return td;
+}
+
 initWapTable = function(r,c) {
-	return {
-		r_cnt: r,
-		c_cnt: c,
-		elem: createTableElem(r,c),
+	var t = {};
+	t.r_cnt= r;
+	t.c_cnt= c;
 
-		r_curr: 0,
-		c_curr: 0,
+	t.r_curr= 0;
+	t.c_curr= 0;
 
-		keyDown: function(keyCode){
-			var newRow=this.r_curr, newCol=this.c_curr;
-
-			if (keyCode == KEYc_LEFT && this.c_curr!=0) {
-				newCol--;
+	t.rows= [];
+	for(let r=0;r<t.r_cnt;r++){
+		let row = {};
+		row.cells=[];
+		for(let c=0;c<t.c_cnt;c++){
+			let cell = {};
+			cell.r = r;
+			cell.c = c;
+			cell.inputText = ""+rand();
+			if(cell.r==0 && cell.c==0){
+				cell.inputText = "=avg(b0,c0)"
 			}
-			else if (keyCode == KEYc_UP && this.r_curr!=0) { 
-				newRow--;
-			}
-			else if (keyCode == KEYc_RIGHT && this.c_curr!=this.c_cnt-1) { 
-				newCol++;
-			}
-			else if (keyCode == KEYc_DOWN && this.r_curr!=this.r_cnt-1) { 
-				newRow++;
-			}
-			else{
-				return false;
-			}
-
-			this.setCurrentCell(newRow,newCol);
-			return true;
-		},
-		getCell: function(r,c){
-			return this.elem.rows[r+1].cells[c+1];
-		},
-		getCurrentCell: function(){
-			return this.getCell(this.r_curr,this.c_curr);
-		},
-		setCurrentCell: function(r,c){
-			var curr_cell = this.getCurrentCell();
-			curr_cell.classList.remove("currentCell");
-			tgt_cell = this.getCell(r,c);
-			tgt_cell.classList.add("currentCell");
-			this.r_curr = r;
-			this.c_curr = c;
-		},
-		getCurrentCellText: function(){
-			var curr_cell = this.getCurrentCell();
-			return curr_cell.innerHTML;
-		},
-		setCurrentCellText: function(text){
-			var curr_cell = this.getCurrentCell();
-			curr_cell.innerHTML = text;
-		},
-		setCellText: function(r,c,text){
-			var cell = this.getCell(r,c);
-			cell.innerHTML = text;
-		},
-		delSelection: function(){
-			var curr_cell = this.getCurrentCell();
-			for(var r=0;r<this.r_cnt;r++){
-				for(var c=0;c<this.c_cnt;c++){
-					var cell = this.getCell(r,c);
-					if(cell.classList.contains("selected")){
-						cell.classList.remove("selected");
-						this.setCellText(r,c,"");
-					}
-				}
-			}
-		},
-		recomputeTable: function(){
-			for(var r=0;r<this.r_cnt;r++){
-				for(var c=0;c<this.c_cnt;c++){
-					var val = this.getCell(r,c).innerHTML;
-					if(val.charAt(0)=='='){
-						this.evaluateCell(r,c,val);
-					}
-				}
-			}
-		},
-		evaluateCell: function(r,c,val){
-			var re_num = "(-?[0-9]+)";
-			var re_cellPtr = "(([a-j]|[A-J])[0-9])";
-			var re_term ="("+re_num+"|"+re_cellPtr+")";
-			var re_op = re_term+"("+"[\\+\\-\\*\\/]"+re_term+")*";
-			var re = re_op
-			console.log(re_op);
-			formula_regex = new RegExp("^="+re+"$","g")
-			let isValidExpr = formula_regex.test(val);
-			if (!isValidExpr){
-				this.setCellText(r,c,"INVALID EXPR");
-				return;
-			}
-
-			cellPtr_regex = new RegExp(re_cellPtr,"g")
-			var match = null;
-			var deref = val;
-			while((match = cellPtr_regex.exec(val))!=null){
-				let m_idx = match.index;
-				let m_rowDesc = match[0].charAt(1);
-				let m_colDesc = match[0].charAt(0);
-				let m_r = parseInt(m_rowDesc);
-				let m_c = parseInt(m_colDesc.charCodeAt(0)) - 97;
-				let m_cellVal = this.getCell(m_r,m_c).innerHTML;
-				console.log(match[0]);
-				deref = deref.replace(new RegExp(match[0],"g"),m_cellVal);
-				console.log(deref);
-			}
-			deref = deref.substring(1); //remove exp flag "="
-			this.setCellText(r,c,eval(deref));
+			cell.elem = createCellElem(cell);
+			row.cells.push(cell);
 		}
+		t.rows.push(row);
 	}
+
+	t.elem= createTableElem(t.rows);
+
+	t.keyDown= function(keyCode){
+		var newRow=t.r_curr, newCol=t.c_curr;
+
+		if (keyCode == KEYc_LEFT && t.c_curr!=0) {
+			newCol--;
+		}
+		else if (keyCode == KEYc_UP && t.r_curr!=0) { 
+			newRow--;
+		}
+		else if (keyCode == KEYc_RIGHT && t.c_curr!=t.c_cnt-1) { 
+			newCol++;
+		}
+		else if (keyCode == KEYc_DOWN && t.r_curr!=t.r_cnt-1) { 
+			newRow++;
+		}
+		else{
+			return false;
+		}
+
+		t.setCurrentCell(newRow,newCol);
+		return true;
+	};
+
+	t.getCell= function(r,c){
+		return t.rows[r].cells[c];
+	};
+
+	t.getCurrentCell= function(){
+		return t.getCell(t.r_curr,t.c_curr);
+	};
+
+	t.getCellInput= function(r,c){
+		return t.getCell(r,c).inputText;
+	}
+
+	t.getCellOutput= function(r,c){
+		return t.getCell(r,c).elem.innerHTML;
+	}
+
+	t.getCurrentCellOutput= function(){
+		var curr_cell = t.getCurrentCell();
+		return curr_cell.elem.innerHTML;
+	};
+
+	t.getCurrentCellInput= function(){
+		return t.getCurrentCell().inputText;
+	};
+
+	t.setCurrentCell= function(r,c){
+		var curr_cell = t.getCurrentCell();
+		curr_cell.elem.classList.remove("currentCell");
+		tgt_cell = t.getCell(r,c);
+		tgt_cell.elem.classList.add("currentCell");
+		t.r_curr = r;
+		t.c_curr = c;
+	};
+
+	t.setCurrentCellInput= function(text){
+		var curr_cell = t.getCurrentCell();
+		curr_cell.inputText = text;
+	};
+
+	t.setCellOutput= function(r,c,text){
+		var cell = t.getCell(r,c);
+		cell.elem.innerHTML = text;
+	};
+
+	t.setCurrentCellOutput= function(text){
+		var curr_cell = t.getCurrentCell();
+		curr_cell.elem.innerHTML = text;
+	};
+
+
+	t.delSelection= function(){
+		var curr_cell = t.getCurrentCell();
+		for(var r=0;r<t.r_cnt;r++){
+			for(var c=0;c<t.c_cnt;c++){
+				var cell = t.getCell(r,c);
+				if(cell.elem.classList.contains("selected")){
+					cell.elem.classList.remove("selected");
+					t.setCellOutput(r,c,"");
+				}
+			}
+		}
+	};
+
+	t.render= function(){
+		for(var r=0;r<t.r_cnt;r++){
+			for(var c=0;c<t.c_cnt;c++){
+				var val = t.getCellInput(r,c);
+				if(val.charAt(0)=='='){
+					t.evaluateCell(r,c,val);
+				}
+				else{
+					t.setCellOutput(r,c,val);
+				}
+			}
+		}
+	};
+
+	t.evaluateCell= function(r,c,val){
+		var re_num = "(-?[0-9]+)";
+		var re_cellPtr = "(([a-j]|[A-J])[0-9])";
+		var re_term = "("+re_num+"|"+re_cellPtr+")";
+		var re_sum = "sum\\("+re_term+"(,"+re_term+")*"+"\\)";
+		var re_avg = "avg\\("+re_term+"(,"+re_term+")*"+"\\)";
+
+		var re_base = "("+re_term+"|"+re_sum+"|"+re_avg+")";
+
+		var re = re_base+"("+"[\\+\\-\\*\\/]"+re_base+")*";
+		
+		var formula_regex = new RegExp("^="+re+"$","g")
+		console.log(val);
+		let isValidExpr = formula_regex.test(val);
+		if (!isValidExpr){
+			t.setCellOutput(r,c,"SYNTAX_ERR");
+			return;
+		}
+
+		// var re_test = new RegExp("sum\\(1,2\\)","g")
+		// var str = "sum(1,2)+6";
+		// console.log(re_test.test(str)+"<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		// return;
+
+		var cellPtr_regex = new RegExp(re_cellPtr,"g")
+		var match = null;
+		var deref = val;
+		while((match = cellPtr_regex.exec(val))!=null){
+			let m_idx = match.index;
+			let m_rowDesc = match[0].charAt(1);
+			let m_colDesc = match[0].charAt(0);
+			let m_r = parseInt(m_rowDesc);
+			let m_c = parseInt(m_colDesc.charCodeAt(0)) - 97;
+			let m_cellVal = t.getCellOutput(m_r,m_c);
+			console.log(match[0]);
+			deref = deref.replace(new RegExp(match[0],"g"),m_cellVal);
+			console.log(deref);
+		}
+
+		var sum_regex = new RegExp(re_sum,"g");
+		match = null;
+		while((match = sum_regex.exec(deref))!=null){
+			let m_noSumHeader = match[0].substring(3);
+			console.log(m_noSumHeader);
+			let m_AdditionFromSum = m_noSumHeader.replace(new RegExp(",","g"),")+(");
+			console.log(m_AdditionFromSum);
+
+			console.log(">>>>>>>>>>>>>>>>"+"\\("+match[0].substring(4,match[0].length-1)+"\\)");
+			var match_regex = new RegExp("sum\\("+match[0].substring(4,match[0].length-1)+"\\)","g");
+			deref = deref.replace(match_regex,m_AdditionFromSum);
+			console.log(deref);
+		}
+
+		var avg_regex = new RegExp(re_avg,"g");
+		match = null;
+		while((match = avg_regex.exec(deref))!=null){
+			let m_noSumHeader = match[0].substring(3);
+			let m_termCnt = (match[0].match(new RegExp(",","g")) || [] ).length+1;
+			let m_AdditionFromSum = m_noSumHeader.replace(new RegExp(",","g"),")+(");
+			m_AdditionFromSum = "("+"("+m_AdditionFromSum+")/"+m_termCnt+")";
+			console.log(m_AdditionFromSum);
+
+			var match_regex = new RegExp("avg\\("+match[0].substring(4,match[0].length-1)+"\\)","g");
+			deref = deref.replace(match_regex,m_AdditionFromSum);
+			console.log(deref);
+		}
+
+		deref = deref.substring(1); //remove exp flag "="
+		t.setCellOutput(r,c,eval(deref));
+		
+	};
+
+	return t;
 }
 
 createCalcElem = function(id){
@@ -220,7 +321,7 @@ initWapCalc = function(r,c){
 
 	wc.keyDown= function(keyCode){
 		if(wc.t.keyDown(keyCode)){
-			wc.cc.setInputText(wc.t.getCurrentCellText());
+			wc.cc.setInputText(wc.t.getCurrentCellInput());
 			return true;
 		}
 
@@ -254,7 +355,7 @@ initWapCalc = function(r,c){
 		var r=parseInt(cellElem.getAttribute("data-row"));
 		var c=parseInt(cellElem.getAttribute("data-col"));
 		wc.t.setCurrentCell(r,c);
-		wc.cc.setInputText(wc.t.getCurrentCellText());
+		wc.cc.setInputText(wc.t.getCurrentCellInput());
 	}
 
 	wc.cc.del_btn.addEventListener("click",function(e) {
@@ -266,13 +367,15 @@ initWapCalc = function(r,c){
 	});
 
 	wc.cc.c_text.addEventListener("input",function (e) {
-		wc.t.setCurrentCellText(wc.cc.getInputText());
+		//wc.t.setCurrentCellOutput(wc.cc.getInputText());
 	});
 
 	wc.init= function(){
 		wc.t.setCurrentCell(0,0);
-		wc.cc.setInputText(wc.t.getCurrentCellText());
-	}
+		wc.cc.setInputText(wc.t.getCurrentCellOutput());
+	};
+	
+	wc.init();
 
 	return wc;
 }
@@ -281,11 +384,19 @@ initWapCalc = function(r,c){
 const KEYc_D=68,KEYc_SPACE=32,KEYc_ENTER=13,KEYc_ESC=27, KEYc_LEFT=37, KEYc_UP=38, KEYc_RIGHT=39, KEYc_DOWN=40;
 document.onkeydown = function(e){
 	if (activePageElem==null){ return true;} //not a keydown for our calc
-
+	
 	if (activePageElem.cc.c_text==document.activeElement) { 
-		if(e.keyCode==KEYc_ESC || e.keyCode==KEYc_ENTER){
+		if(e.keyCode==KEYc_ESC){
 			activePageElem.cc.c_text.blur();
-			activePageElem.t.recomputeTable();
+			activePageElem.cc.setInputText(activePageElem.t.getCurrentCellInput());
+		}
+		else if(e.keyCode==KEYc_ENTER){
+			activePageElem.cc.c_text.blur();
+			let newInput = activePageElem.cc.getInputText();
+			//if (!(newInput == activePageElem.t.getCurrentCellInput())) {
+				activePageElem.t.setCurrentCellInput(newInput);
+				activePageElem.t.render();
+			//}
 		}
 		return true;
 	}
@@ -296,10 +407,12 @@ document.onkeydown = function(e){
 
 	if(e.keyCode==KEYc_SPACE){
 		activePageElem.t.getCurrentCell().classList.toggle("selected");
+		return false;
 	}
 
 	if(e.keyCode==KEYc_ENTER){
 		activePageElem.cc.c_text.focus();
+		return false;
 	}
 
 	if(e.keyCode==KEYc_LEFT || e.keyCode==KEYc_UP || e.keyCode==KEYc_RIGHT || e.keyCode==KEYc_DOWN){
@@ -312,6 +425,7 @@ document.onkeydown = function(e){
 // ******************************************************
 
 
+// TOP-LEVEL COMPONENTS SWITCHING ***********************
 setActivePageElem = function(tgt_wc){
 	unsetActivePageElem();
 	activePageElem = tgt_wc;
@@ -324,17 +438,16 @@ unsetActivePageElem = function(){
 	}
 }
 
-getActivePageElem = function(){
-	return activePageElem;
-}
-
 var activePageElem = null;
+// ******************************************************
 
+
+// MAIN *************************************************
 document.addEventListener('DOMContentLoaded', function () {
 	var container = document.getElementById("container");
 	let wc = initWapCalc(5,3);
-	wc.init();
 	container.appendChild(wc.elem);
 	setActivePageElem(wc)
-	wc.t.recomputeTable();
+	wc.t.render();
 });
+// ******************************************************
